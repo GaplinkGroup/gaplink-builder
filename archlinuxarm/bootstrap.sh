fe2c309a8eefb711680af776d48dab7cdd5e392f
jingjiezhuang <jingjie.zhuang@igg.com>
2019-10-22 15:43:04 +0800

    bump Beta version

--- archlinuxarm/bootstrap.sh
+++ archlinuxarm/bootstrap.sh
@@ -6,7 +6,10 @@ GAPLINK_UI=../gaplink-ui
 
 # TODO: check network
 
-# TODO: root check
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit 1
+fi
 
 # TODO: check pacman-key --init
 
@@ -14,14 +17,19 @@ GAPLINK_UI=../gaplink-ui
 /bin/cp -f $SOURCE_ROOT/etc/pacman.d/mirrorlist /etc/pacman.d/
 
 # install dependency
-pacman -S --noconfirm - < $BOOTSTRAP_ROOT/packages_requirements.txt
+pacman -S --noconfirm - < packages_requirements.txt
+if [ $? -ne 0 ]
+then
+    echo "pacman install error"
+    exit 1
+fi
 
-rm -f $SOURCE_ROOT/etc/systemd/system/multi-user.target/*
-rm -f $SOURCE_ROOT/etc/systemd/system/network-online.target.wants/*
-rm -f $SOURCE_ROOT/etc/systemd/system/timers.target.wants/*
-rm -f $SOURCE_ROOT/etc/fstab
+/bin/rm -f "$SOURCE_ROOT"/etc/systemd/system/multi-user.target.wants/*
+/bin/rm -f "$SOURCE_ROOT"/etc/systemd/system/network-online.target.wants/*
+/bin/rm -f "$SOURCE_ROOT"/etc/systemd/system/timers.target.wants/*
+/bin/rm -f "$SOURCE_ROOT"/etc/fstab
 
-rm -f /etc/systemd/network/*
+/bin/rm -f /etc/systemd/network/*
 
 chown -R root.root $SOURCE_ROOT/etc
 /bin/cp -rf $SOURCE_ROOT/etc/* /etc/
@@ -30,7 +38,7 @@ chown -R root.root $SOURCE_ROOT/etc
 /bin/rm -rf /opt/gaplink-ui
 mkdir -p /opt/gaplink-ui
 /bin/cp -r $GAPLINK_UI/* /opt/gaplink-ui/
-$BOOTSTRAP_ROOT/scripts/enable_pdosqlite.sh
+bash $BOOTSTRAP_ROOT/scripts/enable_pdosqlite.sh
 
 # install ntpclient
 tar xvf $BOOTSTRAP_ROOT/ntpclient_2015_365.tar.gz
@@ -52,12 +60,13 @@ systemctl enable dnsmasq.service
 systemctl enable systemd-networkd.service
 systemctl enable haveged.service
 systemctl enable v2ray.service
-systemctl enable unbound.service
 systemctl enable ip6tables.service
 systemctl enable gaplinkui.service
 
 
 
-# TODO: fstab noatime,nodiratime
+mount -o remount,noatime,nodiratime /
+mount -o remount,noatime,nodiratime /boot
+genfstab / | grep mmcblk >/etc/fstab
 
 # TODO: change root password
